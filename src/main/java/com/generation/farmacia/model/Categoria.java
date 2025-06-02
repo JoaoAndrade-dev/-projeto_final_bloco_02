0f3698a4e69065e3c7e559eda89059a64f7d142a
JoaoAndrade-dev <joao.andrade1610@outlook.com>
2025-06-02 15:20:00 -0300

    fix: rota de mapping de categorias

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,9 +1,15 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +29,11 @@ public class Categoria {
 	@NotBlank
 	@Size(min =5, max = 200)
 	private String descricao;
+	
+	 @OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	 @JsonIgnoreProperties("categoria")
+	 private List<Produto> produtos;
+	 
 	
 	public long getId() {
 		return id;
